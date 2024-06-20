aaaf1408c332f9905a91268f0b7e842a98f2f228
aargeee <211220042@nitdelhi.ac.in>
2024-06-21 04:43:10 +0530

    add category update models with constraints

--- course/admin.py
+++ course/admin.py
@@ -1,1 +1,19 @@
 # Register your models here.
+from django.contrib import admin
+from .models import (
+    Content,
+    Enrollment,
+    CourseTutor,
+    Chapter,
+    Course,
+    TopicCompletionLog,
+    Category,
+)
+
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Enrollment)
+admin.site.register(CourseTutor)
+admin.site.register(Chapter)
+admin.site.register(Content)
+admin.site.register(TopicCompletionLog)
--- course/models.py
+++ course/models.py
@@ -4,16 +4,35 @@ from AcadMe.models import BaseModel
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 from iam.models import AppUser
+from django.core.exceptions import ValidationError
 
 USER = get_user_model()
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=64)
+
+    def __str__(self) -> str:
+        return f"Category {self.name}"
 
 
 class Course(BaseModel):
     name = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=None)
+
+    def __str__(self) -> str:
+        return f"Course {self.name}"
 
 
 class Enrollment(BaseModel):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["course", "learner"], name="unique_learner_in_course"
+            )
+        ]
+
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     learner = models.ForeignKey(USER, on_delete=models.CASCADE)
 
@@ -22,8 +41,18 @@ class Enrollment(BaseModel):
             raise ValueError("Only Students can enroll in a course.")
         super().save(*args, **kwargs)
 
+    def __str__(self) -> str:
+        return f"{self.course.name} - {self.learner.get_username()}"
+
 
 class CourseTutor(BaseModel):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["course", "tutor"], name="unique_tutor_in_course"
+            )
+        ]
+
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     tutor = models.ForeignKey(USER, on_delete=models.CASCADE)
 
@@ -32,15 +61,35 @@ class CourseTutor(BaseModel):
             raise ValueError("Only Students can enroll in a course.")
         super().save(*args, **kwargs)
 
+    def __str__(self) -> str:
+        return f"{self.course.name} - {self.tutor.get_username()}"
+
 
 class Chapter(BaseModel):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["course", "position"], name="unique_position_per_course"
+            )
+        ]
+
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
     position = models.SmallIntegerField(blank=False, null=False)
     isLastChapter = models.BooleanField(default=False)
 
+    def __str__(self) -> str:
+        return f"{self.course.name} > {self.name}"
+
 
 class Content(BaseModel):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["chapter", "position"], name="unique_position_per_chapter"
+            )
+        ]
+
     class ContentType(models.TextChoices):
         TEXT = "TEXT", _("TEXT")
         VIDEO = "VIDEO", _("VIDEO")
@@ -54,6 +103,34 @@ class Content(BaseModel):
     isLastContent = models.BooleanField(default=False)
     chapter = models.ForeignKey(Chapter, on_delete=models.CASCADE)
 
+    def clean(self):
+        super().clean()
+
+        if self.type == self.ContentType.TEXT and not self.textUrl:
+            raise ValidationError(
+                {"textUrl": _("Text URL is required when content type is TEXT.")}
+            )
+        if self.type == self.ContentType.VIDEO and not self.videoUrl:
+            raise ValidationError(
+                {"videoUrl": _("Video URL is required when content type is VIDEO.")}
+            )
+        if self.type == self.ContentType.BOTH:
+            if not self.textUrl:
+                raise ValidationError(
+                    {"textUrl": _("Text URL is required when content type is BOTH.")}
+                )
+            if not self.videoUrl:
+                raise ValidationError(
+                    {"videoUrl": _("Video URL is required when content type is BOTH.")}
+                )
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.chapter.name} > {self.name}"
+
 
 class TopicCompletionLog(BaseModel):
     created_at = models.DateTimeField(verbose_name=_("Created At"), auto_now_add=True)
@@ -64,3 +141,6 @@ class TopicCompletionLog(BaseModel):
         if self.learner.role != AppUser.ROLES.LEARNER:
             raise ValueError("Only Students can enroll in a course.")
         super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.content.name} > {self.learner.get_username()}"
