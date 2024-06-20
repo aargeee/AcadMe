4e0f1bd96152b92d4310e1ee97910d30799e1c0a
aargeee <211220042@nitdelhi.ac.in>
2024-06-21 02:38:23 +0530

    Add IAM and Course app using django startapp

--- AcadMe/settings.py
+++ AcadMe/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local apps
+    "iam",
+    "course",
     # third party apps
     "corsheaders",
     "rest_framework",
